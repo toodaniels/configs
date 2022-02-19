4082b777a6078dfc74db70f2ea92af8155e2c5aa
Daniel Torres <torresestradadaniel@gmail.com>
2022-02-18 20:45:00 -0600

    Add pacman packages

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# configs
+# Configs
+
+## This configuration is tested in the following:
+
+- Manjaro=5.15.21-1
+- Arch=5.16.9-arch1-1
+
+## Install 
+
+```
+    git clone https://github.com/toodaniels/configs.git
+    cd configs
+    chmod +x install.sh
+    ./install.sh
+```
+
+
--- install.sh
+++ install.sh
@@ -1,11 +1,32 @@
-git clone https://github.com/toodaniels/configs.git
+#!/bin/sh
 
-cp ./init.vim ~/.configs/nvim/
+# Update
+sudo pacman -Syu
 
-cp ./agnoster.zsh-theme ~/.oh-my-zsh/themes/
+# System
 
-conda config --set changeps1 false
+sudo pacman -S git base-devel ntfs-3g core/linux-api-headers python-pip gcc evtest
 
-# pacmd set-card-profile <> headset_head_unit 
-# pacmd set-card-profile <> a2dp_sink    
+# Tools
+
+sudo pacman -S gparted spectacle libreoffice-fresh unzip code docker-compose docker
+
+# Aparience
+
+sudo pacman -S zsh-completions zsh latte-dock neofetch ttf-fira-code
+
+# Git
+
+set -- slack-desktop google-chrome obinskit balena-etcher snapd
+while [ $# -gt 0 ]
+do
+        git clone https://aur.archlinux.org/$1
+        cd $1
+        makepkg -is
+        cd ..
+        rm -R $1
+        shift;
+done
+
+
 
